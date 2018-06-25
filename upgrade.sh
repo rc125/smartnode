3e2ecd51444d87257597d51eb2b20111c0995e1e
Markus GH <markus.ghambari@gmail.com>
2018-06-25 23:18:41 +1000

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,13 +4,13 @@
 # Add the following to the crontab (i.e. crontab -e)
 # 0 0 */1 * * ~/smartnode/upgrade.sh
 
-apt update
+# apt update
 
-if apt list --upgradable | grep -v grep | grep smartcashd > /dev/null
-then
-  smartcash-cli stop && sleep 10
-  rm ~/.smartcash/peers.*
-  apt install smartcashd -y && smartcashd&
-else
-  exit
-fi
+# if apt list --upgradable | grep -v grep | grep smartcashd > /dev/null
+# then
+  # smartcash-cli stop && sleep 10
+  # rm ~/.smartcash/peers.*
+  # apt install smartcashd -y && smartcashd&
+# else
+  # exit
+# fi
