8cec39b80190c8a4f1ccfe2a44a00085f0a9a251
Markus GH <markus.ghambari@gmail.com>
2018-06-25 23:20:28 +1000

    Update patch-0.4.sh

--- patches/patch-0.4.sh
+++ patches/patch-0.4.sh
@@ -2,5 +2,5 @@
 cd
 cd smartnode
 
-// Update upgrade.sh
+// Update upgrade.sh to disable automatic updates
 wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh -O upgrade.sh
